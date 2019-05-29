b19134f29eb511fa249112bd36b46f8313e1f53c
Danie Martinoli <daniechris5@gmail.com>
2019-05-28 21:58:30 -0700

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def dictionary
+  words_to_be_substituted = {  hello: 'hi',
+  to: '2', two: '2', too: '2', for: '4', four: '4', be: 'b', you: 'u', at: "@", and: "&"
+}
+end
+
+def word_substituter(tweet)
+  tweet.each do |word|
+    if word == words_to_be_substituted[key]
+      tweet[key] = words_to_be_substituted[key]
+    end
+  end
+end
