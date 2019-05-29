0920ce3b96f624355a650e52187849473c66509a
Danie Martinoli <daniechris5@gmail.com>
2019-05-29 11:57:59 -0700

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,14 +1,20 @@
 # Write your code here.
 def dictionary
-  words_to_be_substituted = {  hello: 'hi',
-  to: '2', two: '2', too: '2', for: '4', four: '4', be: 'b', you: 'u', at: "@", and: "&"
+  substitutions = {  hello: "hi",
+  to: "2", two: "2", too: "2", for: "4", four: "4", be: "b", you: "u", at: "@", and: "&"
 }
 end
 
-def word_substituter(tweet)
-  tweet.each do |word|
-    if word == words_to_be_substituted[key]
-      tweet[key] = words_to_be_substituted[key]
+def word_substituter(string)
+    string.collect do |word|
+    if word == substitutions
+      string.split = substitutions.keys
     end
   end
+  word_substituter(string)
 end
+
+takes string(which first has to be an first array) and evaluates if a word matches the words in substitutions.
+if so we replace word with the shorter word
+
+return the string
