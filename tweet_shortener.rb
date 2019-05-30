79f7ef2992bbf379dbeded7f8b535f554f14d680
Danie Martinoli <daniechris5@gmail.com>
2019-05-29 18:45:04 -0700

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,23 +1,27 @@
 # Write your code here.
 require "pry"
 def dictionary
-  substitutions = {  "hello": "hi",
+  substitutions = {"hello": "hi",
   "to": "2", "two": "2", "too": "2", "for": "4", "four": "4", "be": "b", "you": "u", "at": "@", "and": "&"
 }
 end
 
 def word_substituter(string)
     tweets = string.split
-    tweets.collect do |word|
-    if dictionary.keys.include?(word)
-       dictionary.keys.find {|key| word == key}
-       word = dictionary[word]
-  end
- end
-    return tweets.join(' ')
-  end
+    words = tweets.collect do |word|
+      if dictionary.keys.include?(word.to_sym)
+         dictionary[word.to_sym]
+         #binding.pry
+       else
+         word
+      end
+    end
+  words.join(' ')
+end
 
-#takes string(which first has to be an first array) and evaluates if a word matches the words in substitutions.
-#if so we replace word with the shorter word
-
-#return the string
+def bulk_tweet_shortener(array)
+  array.each do |tweet|
+   new_tweets = word_substituter(tweet)
+   puts new_tweets
+  end
+end
